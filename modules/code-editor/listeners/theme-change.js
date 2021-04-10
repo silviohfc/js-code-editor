2c84074221246e04eb1571a43243f94a8d4bbfca
Silvio Henrique <silviohfc@gmail.com>
2021-04-10 18:36:30 -0300

    feat: add footer

--- modules/code-editor/listeners/theme-change.js
+++ modules/code-editor/listeners/theme-change.js
@@ -1,4 +1,10 @@
 const triggers = document.querySelectorAll('.editor-option')
+const footerLinks = document.querySelectorAll('.footer-link')
+
+const footerColors = {
+  dracula: "#FF79C6",
+  monokai: "#F92672"
+}
 
 function themeChange(editor) {
   for(const trigger of triggers) {
@@ -6,6 +12,8 @@ function themeChange(editor) {
 
     trigger.addEventListener('click', () => {
       editor.setOption('theme', theme)
+
+      footerLinks.forEach(link => link.style.color = footerColors[theme])
     })
   }
 }
